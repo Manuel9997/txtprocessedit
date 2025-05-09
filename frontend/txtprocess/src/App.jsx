3cc4f5b61cfce9dbad47b7e6293a7dac24d69296
Manuel9997 <N00368025@upn.pe>
2025-05-08 19:00:21 -0500

    v3

--- frontend/txtprocess/src/App.jsx
+++ frontend/txtprocess/src/App.jsx
@@ -3,8 +3,6 @@ import "./App.css";
 import MenuBar from "./Menubar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div id="contenedorprimario">
@@ -12,22 +10,52 @@ function App() {
         <div id="prtxtsubir">
           <div id="txtsubir">
             <div id="stxt">Sube tu archivo</div>
-            <input type="file" id="fileInput" accept=".txt" />
-            <Buttonsend />
+            <Inputshow />
           </div>
         </div>
-        <Mostrar />
       </div>
     </>
   );
 }
 
-function Buttonsend() {
-  return <button className="Procesar">Procesar</button>;
+function Inputshow() {
+  const [file, setFile] = useState(null);
+  const [filecontent, setFileContent] = useState("");
+
+  function manejoFile(e) {
+    if (e.target.files[0]) {
+      const selectedFile = e.target.files[0];
+      setFile(selectedFile);
+      const lector = new FileReader();
+      lector.onload = (event) => {
+        setFileContent(event.target.result);
+      };
+
+      lector.readAsText(selectedFile);
+    }
+  }
+
+  return (
+    <div className="shows">
+      <input type="file" id="fileInput" accept=".txt" onChange={manejoFile} />
+      {file && (
+        <div className="show">
+          <p className="Nombretxt">El nombre del archivo es "{file.name}"</p>
+          <p className="Nombretxtc">El contenido es:</p>
+          <p className="Contenido" onClick={(e) => editFile(e)}>
+            {filecontent}
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-function Mostrar() {
-  return <div className="show"></div>;
-}
+const editFile = (e) => {
+  const [file, setFile] = useState[""];
+  const [fileeditado, setFileeditado] = useState[false];
+  const valores = e.target.value;
+  setFile(valores);
+};
 
 export default App;
